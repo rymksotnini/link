a181fbfb687c0bb9bb417c30a2a3ad151379e979
Emna ksontini <ksontiniaa@gmail.com>
2019-11-18 14:36:10 +0100

    cros policy fixed

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,13 @@ var app = express();
 
 var testController = require('./testController');
 app.use('/', testController);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get("/test", (req, res, next) => {
+    res.json(["test","test"]);
+});
 
 module.exports = app;
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import {Observable} from "rxjs/index";
+import {catchError, map} from "rxjs/internal/operators";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http: HttpClient) {}
+
+  gettest(): Observable<String[]> {
+    return this.http.get<String[]>("http://localhost:3000/test");
+  }
+
+  test()
+  {
+    this.gettest().subscribe(
+      res=> console.log("test",res)
+    )
+  }
 }

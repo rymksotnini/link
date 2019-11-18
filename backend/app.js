7e866eaf0e7edfd4ab1c2e1d2183303539c6a8b3
rymksontini <rymksontini@gmail.com>
2019-11-18 12:36:09 +0100

    database connection

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,17 @@ var app = express();
 
 var testController = require('./testController');
 app.use('/', testController);
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'link'
+});
+
+connection.connect((err)=>{
+    if (err)throw err;
+    console.log('Connected!');
+});
 module.exports = app;

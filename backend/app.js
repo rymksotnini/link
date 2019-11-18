3ad991c72133d48fa48743f0718e274b3b796c36
rymksontini <rymksontini@gmail.com>
2019-11-18 12:47:26 +0100

    externalize connection

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,7 @@ var express = require('express');
 var app = express();
 
 var testController = require('./testController');
+var connection = require('./connection');
 app.use('/', testController);
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'link'
-});
-
-connection.connect((err)=>{
-    if (err)throw err;
-    console.log('Connected!');
-});
 module.exports = app;

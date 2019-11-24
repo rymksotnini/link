792477b577422699f647247ff6d734a8262e1824
Meriem Ben Chaaben <meriembenchaaben97@gmail.com>
2019-11-24 16:15:08 +0100

    config sequelize

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,25 @@ var express = require('express');
 var app = express();
 
 var testController = require('./testController');
-var connection = require('./connection');
+//var connection = require('./connection');
+
+//DB Connection :
+
+var sequelize = require('./src/connection');
+sequelize.authenticate().then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 app.use('/', testController);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 app.get("/test", (req, res, next) => {
     res.json(["test","test"]);
 });

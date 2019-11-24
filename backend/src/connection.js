516009fcb3f55c5f2882e89ddead2d5c6135e409
rymksontini <rymksontini@gmail.com>
2019-11-24 20:02:02 +0100

    create and show event

--- backend/src/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("link",'root','root',
-    {host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false
-});
-
-module.exports = sequelize;
-global.sequelize = sequelize;

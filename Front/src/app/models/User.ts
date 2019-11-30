30f8670837dd35109fd25aa17fcd181841d14734
Emna ksontini <ksontiniaa@gmail.com>
2019-11-30 21:25:41 +0100

    merged with master

---
+++ Front/src/app/models/User.ts
@@ -0,0 +1,21 @@
+export class User {
+
+    public id: number;
+    public UserName: string;
+    public email: string;
+    public password: string;
+    public is_organization : boolean;
+    public is_sponsor:boolean;
+    constructor(
+
+    ) {
+        // this.id = id ? id : null;
+        // this.UserName = UserName ? UserName : null;
+        // this.password = password ? password : null;
+        // this.is_organization = is_organization ? is_organization : null;
+        // this.is_sponsor = is_sponsor   ? is_sponsor : null;
+        // this.email = email ? email : null;
+    }
+
+
+}
--- Front/src/app/signup/signup.component.ts
+++ Front/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {NgForm} from "@angular/forms";
+import {User} from "../models/User";
+import {UserService} from "../services/user.service";
 
 @Component({
     selector: 'app-signup',
@@ -7,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
     test : Date = new Date();
-    focus;
-    focus1;
-    focus2;
-    constructor() { }
+    is_Organization : boolean;
+    is_Sponsor: boolean;
+    submitted : boolean = false;
+    user = new User();
 
-    ngOnInit() {}
+    constructor(private activatedRoute :ActivatedRoute,private userService :UserService) {
+        activatedRoute.params.subscribe(parameters => {
+            this.is_Organization= this.activatedRoute.snapshot.paramMap.get("type")=="organization";
+            this.is_Sponsor= !this.is_Organization;
+            console.log(" organization ? " +this.is_Organization +" sponsor ?"+this.is_Sponsor);
+
+
+
+        });
+    }
+
+    ngOnInit() {
+
+    }
+    onSubmit(formulaire : NgForm){
+        console.log("testing");
+        let prefixType :string;
+        this.submitted=formulaire.valid;
+        if(!this.submitted)
+            return  ;
+        if(this.is_Organization){
+             prefixType ="organization"
+        }else if (this.is_Sponsor)
+            prefixType="sponsor";
+        this.user.UserName=formulaire.controls[prefixType+'Name'].value;
+        console.log(formulaire.controls['password'].value);
+        this.user.email=formulaire.controls[prefixType+'Email'].value;
+        this.user.password=formulaire.controls['password'].value;
+        this.user.is_organization=this.is_Organization;
+        this.user.is_sponsor=this.is_Sponsor;
+        console.log(this.user);
+       // this.userService.create(this.user);
+
+    }
+
+
+    reset(formulaire:NgForm){
+        formulaire.resetForm();
+    }
+
+
 }

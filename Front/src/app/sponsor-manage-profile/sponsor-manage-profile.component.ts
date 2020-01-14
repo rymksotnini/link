c10f80c689a54831d945cbdc4ef5dbd4199c93c3
hayfa2310 <hayfa23tayeb@gmail.com>
2020-01-14 21:43:30 +0100

    image profile upload/download + update profile sponsor

--- Front/src/app/sponsor-manage-profile/sponsor-manage-profile.component.ts
+++ Front/src/app/sponsor-manage-profile/sponsor-manage-profile.component.ts
@@ -3,6 +3,7 @@ import {Sponsor} from "../models/Sponsor";
 import {SponsorService} from "../services/sponsor.service";
 import {NgForm} from "@angular/forms";
 import {LoginService} from "../services/login.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-sponsor-manage-profile',
@@ -12,18 +13,51 @@ import {LoginService} from "../services/login.service";
 export class SponsorManageProfileComponent implements OnInit {
 
   sponsor = new Sponsor();
+  loggedSponsor = new Sponsor();
+  private image_file;
+  private image_name;
 
-  constructor(private sponsorService : SponsorService) { }
+  constructor(private sponsorService : SponsorService,
+              private loginService : LoginService,
+              private http : HttpClient) {
+  }
 
   ngOnInit() {
-    this.sponsorService.getSponsor(7)
-        .subscribe(sponsor => this.sponsor = sponsor);
-    console.log(this.sponsor);
+    this.loginService.getCurrentUser().subscribe((user) => {
+      this.sponsorService.getSponsorByUserId(user.body.id).subscribe((res) => {
+        this.loggedSponsor = res;
+        this.sponsorService.getSponsor(this.loggedSponsor.id)
+            .subscribe((sponsor) => this.sponsor = sponsor);
+      })
+    });
+  }
+
+  selectImage(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.image_file = file;
+    }
   }
 
   save(){
+    this.sponsor.image = this.image_name;
     this.sponsorService.updateSponsor(this.sponsor).subscribe(
         res=> console.log("sponsor updated",res)
     )
   }
+
+  submit(){
+    const formData = new FormData();
+    formData.append('file', this.image_file);
+
+    this.http.post<any>('http://localhost:3000/file', formData).subscribe(
+        (res) =>
+        {
+          this.image_name = res.name;
+          this.save()
+        } ,
+        (err) => console.log(err)
+    );
+  }
+
 }
--- Front/src/app/sponsor-profile/sponsor-profile.component.ts
+++ Front/src/app/sponsor-profile/sponsor-profile.component.ts
@@ -3,6 +3,8 @@ import {Sponsor} from "../models/Sponsor";
 import {SponsorService} from "../services/sponsor.service";
 import {ActivatedRoute} from "@angular/router";
 import {LoginService} from "../services/login.service";
+import {HttpClient} from "@angular/common/http";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-sponsor-profile',
@@ -14,10 +16,13 @@ export class SponsorProfileComponent implements OnInit {
     sponsor = new Sponsor();
     loggedSponsor = new Sponsor();
     isCurrentUser: boolean;
+    imageUrl : any ;
 
     constructor(private sponsorService: SponsorService,
                 private route: ActivatedRoute,
-                private loginService: LoginService) {
+                private loginService: LoginService,
+                private http : HttpClient,
+                private sanitizer : DomSanitizer) {
     }
 
     ngOnInit() {
@@ -35,8 +40,20 @@ export class SponsorProfileComponent implements OnInit {
                             this.isCurrentUser = false;
                         }
                     })
-                })
+                });
+                this.getImage(this.sponsor);
             })
         });
+    }
+
+    getImage(sponsor : Sponsor)
+    {
+        this.http.get("http://localhost:3000/download/" + sponsor.image, { responseType: "blob" })
+            .toPromise()
+            .then((blob) => {
+                this.imageUrl= URL.createObjectURL(blob);
+                sponsor.image = this.sanitizer.bypassSecurityTrustResourceUrl(this.imageUrl)
+            })
+            .catch(err => console.error("download error = ", err))
     }
 }
